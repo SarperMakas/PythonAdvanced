c46c39b5f135e8c05662e1022c66221f5d904f43
Sarper Makas <sarper.makas@superonline.com>
2023-12-19 22:36:46 +0300

    threading 2

---
+++ Threading/learn_threading2.py
@@ -0,0 +1,24 @@
+import threading
+import time
+
+start = time.perf_counter()
+
+def do_something(seconds):
+    print(f"Sleep {seconds} second...")
+    time.sleep(seconds)
+    print("Done Sleeping...")
+
+threads = []
+
+for _ in range(10):
+    t = threading.Thread(target=do_something, args=[1.5])
+    t.start()
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
+
+end = time.perf_counter()
+
+print(f"Finished in {round(end-start, 2)} second(s)")
+
