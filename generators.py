e184e62ccf92eec837d7d9b654eb97dd24a0aa61
Sarper Makas <sarper.makas@superonline.com>
2023-12-19 22:11:25 +0300

    generator

--- generators.py
+++ generators.py
@@ -10,3 +10,33 @@ print(next(generator))
 print(next(generator))
 print(next(generator))
 print(next(generator))
+
+class FibonacciGenerator:
+    def __init__(self, limit):
+        self.first = 1
+        self.second = 2
+        self.counter = 1
+
+        self.limit = limit
+
+    def __iter__(self):
+
+        return self.counter
+
+    def __next__(self):
+        if self.counter >= self.limit:
+            raise StopIteration
+
+        result = self.first
+        self.first, self.second = self.second, self.first + self.second
+        self.counter += 1
+        return result
+print()
+
+generator = FibonacciGenerator(10)
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
+print(next(generator))
