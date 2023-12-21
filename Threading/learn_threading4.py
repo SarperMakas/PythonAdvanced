b331759c56ba0c45d2a21b4d263ff15eb859121f
Sarper Makas <sarper.makas@superonline.com>
2023-12-21 21:49:57 +0300

    multiprocessing 6

--- Threading/learn_threading4.py
+++ Threading/learn_threading4.py
@@ -10,8 +10,6 @@ img_urls = [
     'https://images.unsplash.com/photo-1564135624576-c5c88640f235',
     'https://images.unsplash.com/photo-1541698444083-023c97d3f4b6',
     'https://images.unsplash.com/photo-1522364723953-452d3431c267',
-    'https://images.unsplash.com/photo-1513938709626-033611b8cc03',
-    'https://images.unsplash.com/photo-1507143550189-fed454f93097',
     'https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e',
     'https://images.unsplash.com/photo-1504198453319-5ce911bafcde',
     'https://images.unsplash.com/photo-1530122037265-a5f1f91d3b99',
@@ -27,7 +25,7 @@ def download_images(img_url):
     img_bytes = requests.get(img_url).content
     img_name = img_url.split('/')[3]
     img_name = f'{img_name}.jpg'
-    with open(img_name, 'wb') as img_file:
+    with open(f"processed\\{img_name}", 'wb') as img_file:
         img_file.write(img_bytes)
         print(f'{img_name} was downloaded...')
 
